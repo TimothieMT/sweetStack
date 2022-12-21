1c572f841dd3dc56a2be9b9455cdbd1feb97f981
TimothieMT <ttolk89@gmail.com>
2022-12-21 23:35:15 +0100

    cli interface

--- bin/index.js
+++ bin/index.js
@@ -1,14 +1,102 @@
-#! /usr/bin/env node
+#!/usr/bin/env node
 
-const { Command } = require('commander');
-const program = new Command();
+import inquirer from "inquirer";
+import {Command, Option} from "commander";
+import fs from 'fs';
 
-program
-    .name('sweetstack')
-    .description('Create your own complete web project')
-    .version('beta');
 
-program.command('create')
-    .description('Choose your programming language: ')
+const app = new Command();
 
-program.parse();
+//variables
+const name = "cli"
+
+const questions = [
+    {type: "input", name: 'name', message: 'project name:'},
+    {type: "list", name: 'frontend', choices: ['react', 'vue', 'angular'], message: 'selected frontend:'},
+    {type: "list", name: 'backend', choices: ['yes', 'no'], message: 'need a backend ?'}
+]
+
+const filename = () => {
+    inquirer.prompt(questions).then(((answers) => {
+
+        if (answers.name !== ' ' && answers['frontend'] === 'react' && answers['backend'] === 'no') {
+            const path = './' + answers.name
+            fs.mkdir(path, (err) => {
+                if (err) {
+                    console.log("error occurred in creating new directory", err);
+                    return;
+                }
+                console.log("New directory created successfully");
+            })
+        } else {
+            console.log(answers)
+        }
+    }))
+}
+app
+    .name(name)
+    .description('create' + name)
+    .version('1.0.0')
+    .action(filename)
+
+
+app.parse(process.argv);
+// program.command('create')
+//     .description('create new project')
+//     .argument('<project_name>', 'project name')
+// .action((project_name) => {
+//     const path = './' + project_name
+//     fs.mkdir(path, (err) => {
+//         if (err) {
+//             console.log("error occurred in creating new directory", err);
+//             return;
+//         }
+//         console.log("New directory created successfully");
+//     })
+// })
+//
+// program.command('react')
+//     .description('react frontend')
+//     .argument('-r, --react', 'react')
+//     .action((react) => {
+//         console.log(react)
+//     })
+//
+// program.command('vue')
+//     .description('vue frontend')
+//     .argument('-v, --vue', 'vue')
+//     .action((vue) => {
+//         console.log(vue)
+//     })
+//
+// program
+//     .name('angular')
+//     .command('angular')
+//     .description('angular frontend')
+//     .argument('-a, --angular', 'angular')
+//     .action((angular) => {
+//         console.log(angular)
+//     })
+// program.command('backend')
+//     .description('backend')
+//     .argument('-b, --backend', 'backend')
+//     .action((backend) => {
+//         console.log('backend')
+//     })
+
+// .option('-b, --backend', 'backend')
+// .argument('react , vue, angular','frontend')
+// .argument('-r, --react', 'react frontend')
+// .action((react) => {
+//     console.log('react')
+// })
+
+// .argument('-v, --vue', 'react frontend')
+// .action((vue) => {
+//     console.log('vue')
+// })
+
+// const options = program.opts()
+
+
+
