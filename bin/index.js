da5b39dd1fec56e9e9347addef5385926092e949
TimothieMT <ttolk89@gmail.com>
2022-12-27 22:26:37 +0100

    edit if question

--- bin/index.js
+++ bin/index.js
@@ -17,7 +17,7 @@ const __dirname = path.dirname(__filename);
 const questions = [{type: "input", name: 'name', message: chalk.hex('#a08c95').bold('project name:')}, {
     type: "list",
     name: 'frontend',
-    choices: [chalk.blue('react'), chalk.green('vue'),chalk.red('angular')],
+    choices: [chalk.blue('react'), chalk.green('vue'), chalk.hex('#ff7247')('angular')],
     message: chalk.hex('#a08c95').bold('selected frontend: ')
 }, {type: "list", name: 'backend', choices: ['yes', 'no'], message: chalk.hex('#a08c95').bold('need a backend? ')}]
 
@@ -25,7 +25,6 @@ const questions = [{type: "input", name: 'name', message: chalk.hex('#a08c95').b
 async function copyAFile(from, to) {
     try {
         await copyFile(from, to);
-        console.log(`Copied ${from} to ${to}`);
     } catch (err) {
         console.error(`Got an error trying to copy the file: ${err.message}`);
     }
@@ -39,7 +38,7 @@ async function copyAll(fromDir, toDir, filePaths) {
 
 const createProject = () => {
 
-    //CREATE REACT FRONTEND AND BACKEND
+    //CREATE REACT FRONTEND
     inquirer.prompt(questions).then(((answers) => {
 
         const srcFile = `${process.cwd()}/${answers.name}`;
@@ -48,7 +47,7 @@ const createProject = () => {
 
             fs.mkdir(srcFile, (err) => {
                 if (err) {
-                    console.log("error occurred in creating new directory", err);
+                    console.log("error occurred in creating new directory", chalk.red(err));
                 }
                 fs.mkdirSync(srcFile + "/frontend")
                 fs.mkdirSync(srcFile + "/frontend/src")
@@ -63,43 +62,44 @@ const createProject = () => {
                     `${srcFile}/frontend/src`,
                     ['App.scss', 'App.tsx', 'index.css', 'main.tsx', 'vite-env.d.ts'],
                 ).then(r => r);
-
-                console.log("New directory created successfully");
-
-                if (answers['backend'] === 'yes') {
-                    fs.mkdirSync(srcFile + "/backend")
-                    fs.mkdirSync(srcFile + "/backend/dist")
-                    fs.mkdirSync(srcFile + "/backend/src")
-
-                    copyAll(
-                        `${path.join(__dirname, "../")}/templates/react-backend`,
-                        `${srcFile}/backend`,
-                        ["nodemon.json", "package.json", "package-lock.json", "tsconfig.json"],
-                    ).then(r => r);
-                    copyAll(
-                        `${path.join(__dirname, "../")}/templates/react-backend/dist`,
-                        `${srcFile}/backend/dist`,
-                        ["server.js", "server.js.map"],
-                    ).then(r => r);
-                    copyAll(
-                        `${path.join(__dirname, "../")}/templates/react-backend/src`,
-                        `${srcFile}/backend/src`,
-                        ["server.ts"],
-                    ).then(r => r);
-                }
             });
-        } else {
-            console.log(answers)
+
+            console.log(chalk.green('react frontend successfully!'))
+
         }
+
+        //CREATE REACT BACKEND
+        if (answers.name !== '' && answers['frontend'] === chalk.blue('react') && answers['backend'] === 'yes') {
+            fs.mkdirSync(srcFile + "/backend")
+            fs.mkdirSync(srcFile + "/backend/dist")
+            fs.mkdirSync(srcFile + "/backend/src")
+
+            copyAll(
+                `${path.join(__dirname, "../")}/templates/react-backend`,
+                `${srcFile}/backend`,
+                ["nodemon.json", "package.json", "package-lock.json", "tsconfig.json"],
+            ).then(r => r);
+            copyAll(
+                `${path.join(__dirname, "../")}/templates/react-backend/dist`,
+                `${srcFile}/backend/dist`,
+                ["server.js", "server.js.map"],
+            ).then(r => r);
+            copyAll(
+                `${path.join(__dirname, "../")}/templates/react-backend/src`,
+                `${srcFile}/backend/src`,
+                ["server.ts"],
+            ).then(r => r);
+            console.log(chalk.green('react backend successfully!'))
+        }
+
+        console.log(chalk.red("ERROR"))
+
     }))
-
 }
-
 app
+
     .name(name)
     .description('create' + name)
     .version('1.0.0')
     .action(createProject)
-
-
 app.parse(process.argv);
