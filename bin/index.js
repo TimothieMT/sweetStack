2432a2b6f899008253e168a70a1fdd3a4db0e8c5
TimothieMT <ttolk89@gmail.com>
2023-01-04 11:39:32 +0100

    code optimization

--- bin/index.js
+++ bin/index.js
@@ -17,11 +17,6 @@ const name = 'sweetstack'
 
 //REQUESTS
 const questions = [
-    {
-        type: "input",
-        name: 'path',
-        message: chalk.hex('#a08c95').bold('please enter the path to "/../node_modules" folder: ')
-    },
     {type: "input", name: 'name', message: chalk.hex('#a08c95').bold('project name:')},
     {
         type: "list",
@@ -31,6 +26,7 @@ const questions = [
     },
     {type: "list", name: 'backend', choices: ['yes', 'no'], message: chalk.hex('#a08c95').bold('need a backend? ')}
 ]
+
 
 //COPY A FILE
 async function copyAFile(from, to) {
@@ -62,7 +58,7 @@ function absPath() {
     try {
         switch (process.platform) {
             case 'win32':
-                if (fs.existsSync(winPath1)) {
+                if (process.platform === 'win32' && fs.existsSync(winPath1)) {
                     result = winPath1
                 } else {
                     result = winPath2
@@ -76,7 +72,8 @@ function absPath() {
                 }
                 break
             case 'darwin':
-                return macPath1
+                result = macPath1
+                break;
             default: {
                 console.log(chalk.red('your platform is not supported'))
             }
@@ -120,135 +117,162 @@ function expressBackend(from, to, name) {
 }
 
 
-//CREATE FUNCTION
-const createProject = () => {
+const reactFrontend = (from, to, answers) => {
+    //CREATE REACT FRONTEND
+    if (answers.name !== '' && answers['frontend'] === chalk.hex('#A7C7E7')('react')) {
 
-    inquirer.prompt(questions).then(((answers) => {
+        fs.mkdirSync(to)
+        fs.mkdirSync(to + "/frontend")
+        fs.mkdirSync(to + "/frontend/src")
+        fs.mkdirSync(to + "/frontend/public")
 
-        let absolutePath = ''
+        copyAll(
+            `${from}/templates/react-frontend`,
+            `${to}/frontend`,
+            ['package.json', 'index.html', 'package-lock.json', 'tsconfig.json', 'tsconfig.node.json', 'vite.config.ts'],
+        ).then(r => r);
+        copyAll(
+            `${from}/templates/react-frontend/src`,
+            `${to}/frontend/src`,
+            ['App.scss', 'App.tsx', 'index.css', 'main.tsx', 'vite-env.d.ts'],
+        ).then(r => r);
 
-            if (answers['path'] === null || answers['path'] === undefined || answers['path'] === '' ) {
-                absolutePath = absPath()
-            } else {
-                absolutePath = `${answers.path}/${name}/`
-            }
-
-        const destPath = `${path.resolve()}/${answers.name}`
-
-
-        fs.mkdirSync(destPath)
-        fs.mkdirSync(destPath + "/frontend")
-        fs.mkdirSync(destPath + "/frontend/src")
-        fs.mkdirSync(destPath + "/frontend/public")
-
-        //CREATE REACT FRONTEND
-        if (answers.name !== '' && answers['frontend'] === chalk.hex('#A7C7E7')('react')) {
-
-            copyAll(
-                `${absolutePath}/templates/react-frontend`,
-                `${destPath}/frontend`,
-                ['package.json', 'index.html', 'package-lock.json', 'tsconfig.json', 'tsconfig.node.json', 'vite.config.ts'],
-            ).then(r => r);
-            copyAll(
-                `${absolutePath}/templates/react-frontend/src`,
-                `${destPath}/frontend/src`,
-                ['App.scss', 'App.tsx', 'index.css', 'main.tsx', 'vite-env.d.ts'],
-            ).then(r => r);
-
-            console.log(chalk.green(`react frontend successfully!
+        console.log(chalk.green(`react frontend successfully!
             
             cd ${answers.name}
             cd frontend
             npm install
             npm run dev`))
-        }
+    }
+}
 
-        //CREATE VUE FRONTEND
-        if (answers.name !== '' && answers['frontend'] === chalk.green('vue')) {
+function vueFrontend(from, to, answers) {
+    //CREATE VUE FRONTEND
+    if (answers.name !== '' && answers['frontend'] === chalk.green('vue')) {
 
-            fs.mkdirSync(destPath + "/frontend/src/assets")
-            fs.mkdirSync(destPath + "/frontend/src/components")
-            fs.mkdirSync(destPath + "/frontend/src/components/icons")
-            fs.mkdirSync(destPath + "/frontend/src/router")
-            fs.mkdirSync(destPath + "/frontend/src/views")
+        fs.mkdirSync(to)
+        fs.mkdirSync(to + "/frontend")
+        fs.mkdirSync(to + "/frontend/src")
+        fs.mkdirSync(to + "/frontend/public")
+        fs.mkdirSync(to + "/frontend/src/assets")
+        fs.mkdirSync(to + "/frontend/src/components")
+        fs.mkdirSync(to + "/frontend/src/components/icons")
+        fs.mkdirSync(to + "/frontend/src/router")
+        fs.mkdirSync(to + "/frontend/src/views")
 
-            copyAll(
-                `${absolutePath}/templates/vue-frontend`,
-                `${destPath}/frontend`,
-                ['env.d.ts', 'index.html', 'package.json', 'package-lock.json', 'README.md', 'tsconfig.config.json', 'tsconfig.json', 'vite.config.ts'],
-            ).then(r => r);
-            copyAll(
-                `${absolutePath}/templates/vue-frontend/src`,
-                `${destPath}/frontend/src`,
-                ['App.vue', 'main.ts'],
-            ).then(r => r);
-            copyAll(
-                `${absolutePath}/templates/vue-frontend/src/assets`,
-                `${destPath}/frontend/src/assets`,
-                ['base.css', 'logo.svg', 'main.css'],
-            ).then(r => r);
-            copyAll(
-                `${absolutePath}/templates/vue-frontend/src/components`,
-                `${destPath}/frontend/src/components`,
-                ['HelloWorld.vue', 'TheWelcome.vue', 'WelcomeItem.vue'],
-            ).then(r => r);
-            copyAll(
-                `${absolutePath}/templates/vue-frontend/src/components/icons`,
-                `${destPath}/frontend/src/components/icons`,
-                ['IconCommunity.vue', 'IconDocumentation.vue', 'IconEcosystem.vue', 'IconSupport.vue', 'IconTooling.vue'],
-            ).then(r => r);
-            copyAll(
-                `${absolutePath}/templates/vue-frontend/src/router`,
-                `${destPath}/frontend/src/router`,
-                ['index.ts'],
-            ).then(r => r);
-            copyAll(
-                `${absolutePath}/templates/vue-frontend/src/views`,
-                `${destPath}/frontend/src/views`,
-                ['AboutView.vue', 'HomeView.vue'],
-            ).then(r => r);
+        copyAll(
+            `${from}/templates/vue-frontend`,
+            `${to}/frontend`,
+            ['env.d.ts', 'index.html', 'package.json', 'package-lock.json', 'README.md', 'tsconfig.config.json', 'tsconfig.json', 'vite.config.ts'],
+        ).then(r => r);
+        copyAll(
+            `${from}/templates/vue-frontend/src`,
+            `${to}/frontend/src`,
+            ['App.vue', 'main.ts'],
+        ).then(r => r);
+        copyAll(
+            `${from}/templates/vue-frontend/src/assets`,
+            `${to}/frontend/src/assets`,
+            ['base.css', 'logo.svg', 'main.css'],
+        ).then(r => r);
+        copyAll(
+            `${from}/templates/vue-frontend/src/components`,
+            `${to}/frontend/src/components`,
+            ['HelloWorld.vue', 'TheWelcome.vue', 'WelcomeItem.vue'],
+        ).then(r => r);
+        copyAll(
+            `${from}/templates/vue-frontend/src/components/icons`,
+            `${to}/frontend/src/components/icons`,
+            ['IconCommunity.vue', 'IconDocumentation.vue', 'IconEcosystem.vue', 'IconSupport.vue', 'IconTooling.vue'],
+        ).then(r => r);
+        copyAll(
+            `${from}/templates/vue-frontend/src/router`,
+            `${to}/frontend/src/router`,
+            ['index.ts'],
+        ).then(r => r);
+        copyAll(
+            `${from}/templates/vue-frontend/src/views`,
+            `${to}/frontend/src/views`,
+            ['AboutView.vue', 'HomeView.vue'],
+        ).then(r => r);
 
-            console.log(chalk.green(`vue backend successfully!
+        console.log(chalk.green(`vue backend successfully!
             
             cd ${answers.name}
             cd frontend
             npm install
             npm run dev`))
-        }
+    }
+}
 
-        //CREATE ANGULAR FRONTEND
-        if (answers.name !== '' && answers['frontend'] === chalk.hex('#ff7247')('angular')) {
 
-            fs.mkdirSync(destPath + '/frontend/src/app')
-            fs.mkdirSync(destPath + '/frontend/src/assets')
+function angularFrontend(from, to, answers) {
+    //CREATE ANGULAR FRONTEND
+    if (answers.name !== '' && answers['frontend'] === chalk.hex('#ff7247')('angular')) {
 
-            copyAll(
-                `${absolutePath}/templates/angular-frontend`,
-                `${destPath}/frontend`,
-                ['angular.json', 'package.json', 'package-lock.json', 'README.md', 'tsconfig.app.json', 'tsconfig.spec.json', 'tsconfig.json'],
-            ).then(r => r);
-            copyAll(
-                `${absolutePath}/templates/angular-frontend/src`,
-                `${destPath}/frontend/src`,
-                ['styles.sass', 'index.html', 'favicon.ico', 'main.ts'],
-            ).then(r => r);
-            copyAll(
-                `${absolutePath}/templates/angular-frontend/src/assets`,
-                `${destPath}/frontend/src/assets`,
-                ['.gitkeep'],
-            ).then(r => r);
-            copyAll(
-                `${absolutePath}/templates/angular-frontend/src/app`,
-                `${destPath}/frontend/src/app`,
-                ['app.component.html', 'app.component.sass', 'app.component.ts', 'app.module.ts', 'app-routing.module.ts'],
-            ).then(r => r);
 
-            console.log(chalk.green(`angular backend successfully!
+        fs.mkdirSync(to)
+        fs.mkdirSync(to + "/frontend")
+        fs.mkdirSync(to + "/frontend/src")
+        fs.mkdirSync(to + "/frontend/public")
+        fs.mkdirSync(to + '/frontend/src/app')
+        fs.mkdirSync(to + '/frontend/src/assets')
+
+        copyAll(
+            `${from}/templates/angular-frontend`,
+            `${to}/frontend`,
+            ['angular.json', 'package.json', 'package-lock.json', 'README.md', 'tsconfig.app.json', 'tsconfig.spec.json', 'tsconfig.json'],
+        ).then(r => r);
+        copyAll(
+            `${from}/templates/angular-frontend/src`,
+            `${to}/frontend/src`,
+            ['styles.sass', 'index.html', 'favicon.ico', 'main.ts'],
+        ).then(r => r);
+        copyAll(
+            `${from}/templates/angular-frontend/src/assets`,
+            `${to}/frontend/src/assets`,
+            ['.gitkeep'],
+        ).then(r => r);
+        copyAll(
+            `${from}/templates/angular-frontend/src/app`,
+            `${to}/frontend/src/app`,
+            ['app.component.html', 'app.component.sass', 'app.component.ts', 'app.module.ts', 'app-routing.module.ts'],
+        ).then(r => r);
+
+        console.log(chalk.green(`angular backend successfully!
             
             cd ${answers.name}
             cd frontend
             npm install
             ng serve`))
+    }
+}
+
+//CREATE FUNCTION
+const createProject = () => {
+
+    inquirer.prompt(questions).then(((answers) => {
+
+        let absolutePath = absPath()
+        const destPath = `${path.resolve()}/${answers.name}`
+
+        if (copyAFile) {
+
+            console.log(chalk.red('Path not found'))
+
+            inquirer.prompt({
+                type: "input",
+                name: 'path',
+                message: chalk.hex('#a08c95').bold('please enter the path to "/../node_modules" folder manually: ')
+            }).then ((answer) => {
+
+            absolutePath = `${answer.path}/${name}/`
+
+            //CREATE FRONTEND RECAT VUE AND ANGULAR
+            reactFrontend(absolutePath, destPath, answers)
+            vueFrontend(absolutePath, destPath, answers)
+            angularFrontend(absolutePath, destPath, answers)
+            })
         }
 
         //CREATE BACKEND REACT AND VUE
@@ -260,7 +284,6 @@ const createProject = () => {
         }
     }))
 }
-
 
 app
     .action(createProject)
