2b2f1a1f06664bbc11a64f84d0daae967eaae3f4
TimothieMT <ttolk89@gmail.com>
2023-01-07 23:14:03 +0100

    hook function implemented

--- test/frontend/src/App.tsx
+++ test/frontend/src/App.tsx
@@ -1,3 +1,16 @@
+import './App.scss'
+
+function App() {
+
+  return (
+    <div className="App">
+      <h1>Welcome to SWEETSTACK</h1>
+        <p>Visit <a href={'#'} >documentary</a> </p>
+    </div>
+  )
+}
+
+export default App
 /*
 
 
