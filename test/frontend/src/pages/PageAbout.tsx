284aeabc9ad1e7851dad74aa2e6b51bc3e518969
TimothieMT <ttolk89@gmail.com>
2023-01-24 12:41:51 +0100

    working on hooks

--- test/frontend/src/pages/PageAbout.tsx
+++
@@ -1,7 +0,0 @@
-export const PageAbout = () => {
-	return (
-		<>
-			<p>This is the about page.</p>
-		</>
-	);
-};
--- test/frontend/src/pages/PageInfo.tsx
+++
@@ -1,7 +0,0 @@
-export const PageInfo = () => {
-	return (
-		<>
-			<p>This is the info page.</p>
-		</>
-	);
-};
