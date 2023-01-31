4b57302c19020297f49853d7c66b4384093927b2
TimothieMT <ttolk89@gmail.com>
2023-01-31 17:28:03 +0100

    create database

--- test/test-backend/src/tools.ts
+++
@@ -1,6 +0,0 @@
-import bcrypt from 'bcrypt';
-
-export const passwordIsCorrect = async (password: string, hash: string) => {
-	const isCorrect = await bcrypt.compare(password, hash);
-	return isCorrect;
-}
